f6ffee2ca3ef652ba36144fa3cc3de25c482e8d8
TomBitDe <tbit455674@gmail.com>
2022-10-03 09:21:41 +0200

    Docu extended.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
@@ -1,1 +1,16 @@
+/**
+ * Entity model for Simple Warehouse application.
+ * <p>
+ * <pre>{@code
+ * 
+ *      +--------------+                         +----------------+
+ *      | HandlingUnit |                         | Location       |
+ *      |              |                         |                |
+ *      |  id          |                         |  locationId    |
+ *      |  location    | <-- 0:1 ------- 0:N --> |  handlingUnits |
+ *      |  ...         |                         |  ...           |
+ *      +--------------+                         +----------------+
+ *      
+ * }</pre> 
+ */
 package com.home.simplewarehouse.model;
