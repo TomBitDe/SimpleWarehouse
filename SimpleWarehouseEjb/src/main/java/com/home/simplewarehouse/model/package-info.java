a64249ddb6b33a9a15cae91704d9a99149e74114
TomBitDe <tbit455674@gmail.com>
2022-10-06 19:07:55 +0200

    Model docu extended.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
@@ -3,26 +3,26 @@
  *
  * <pre>{@code
  * 
- *      +--------------+                         +-----------------+
- *      | HandlingUnit |                         | Location        |
- *      |              |                         |                 |
- *      |  id          |                         |  locationId     |<-----+
- *      |  location    | <-- 0:1 ------- 0:N --> |  handlingUnits  |      |
- *      |  ...         |                         |  locationStatus |      |
- *      +--------------+                         |  ...            |      |      
- *                                               +-----------------+      |
- *                                                                        |
- *                                                                        |
- *                                                                       1:1
- *                                                                        |
- *                                                                        v
- *                                                                  +----------------+
- *                                                                  | LocationStatus |
- *                                                                  |                |
- *                                                                  |  locationId    |
- *                                                                  |  errorStatus   |
- *                                                                  |  ...           |
- *                                                                  +----------------+
+ *      +--------------+                         +--------------------------+
+ *      | HandlingUnit |                         | Location                 |
+ *      |              |                         |                          |
+ *      |  id          |                         |  locationId              |<-----+---------------------+
+ *      |  location    | <-- 0:1 ------- 0:N --> |  handlingUnits           |      |                     |
+ *      |  ...         |                         |  locationStatus          |      |                     |
+ *      +--------------+                         |  locationCharacteristics |      |                     |    
+ *                                               |  ...                     |      |                     |
+ *                                               |                          |      |                     |
+ *                                               +--------------------------+      |                     |
+ *                                                                                1:1                   1:1
+ *                                                                                 |                     |
+ *                                                                                 v                     v
+ *                                                                           +----------------+    +-------------------------+
+ *                                                                           | LocationStatus |    | LocationCharacteristics |
+ *                                                                           |                |    |                         |
+ *                                                                           |  locationId    |    |  locationId             |
+ *                                                                           |  errorStatus   |    |  capacity               |
+ *                                                                           |  ...           |    |  ...                    |
+ *                                                                           +----------------+    +-------------------------+
  *                                                                  
  * }</pre>
  */
