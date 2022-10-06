68b92edeaf6860b15132e47bc5c1d1b50cd7a297
TomBitDe <tbit455674@gmail.com>
2022-10-06 18:15:48 +0200

    Added test.

---
+++ SimpleWarehouseEjb/src/test/java/com/home/simplewarehouse/location/LocationCharacteristicsBeanTest.java
@@ -0,0 +1,144 @@
+package com.home.simplewarehouse.location;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.home.simplewarehouse.handlingunit.HandlingUnitBean;
+import com.home.simplewarehouse.handlingunit.HandlingUnitLocal;
+import com.home.simplewarehouse.model.EntityBase;
+import com.home.simplewarehouse.model.Location;
+import com.home.simplewarehouse.model.LocationCharacteristics;
+import com.home.simplewarehouse.utils.telemetryprovider.monitoring.PerformanceAuditor;
+import com.home.simplewarehouse.utils.telemetryprovider.monitoring.boundary.MonitoringResource;
+
+/**
+ * Test the location characteristics bean.
+ */
+@RunWith(Arquillian.class)
+public class LocationCharacteristicsBeanTest {
+	private static final Logger LOG = LogManager.getLogger(LocationCharacteristicsBeanTest.class);
+
+	@EJB
+	LocationCharacteristicsLocal locationCharacteristicsLocal;
+	
+	@EJB
+	LocationLocal locationLocal;
+	
+	@Deployment
+	public static JavaArchive createTestArchive() {
+		LOG.trace("--> createTestArchive()");
+		JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test.jar")
+				/* Put the test-*.xml in JARs META-INF folder as *.xml */
+				.addAsManifestResource(new File("src/test/resources/META-INF/test-persistence.xml"), "persistence.xml")
+				.addAsManifestResource(new File("src/test/resources/META-INF/test-ejb-jar.xml"), "ejb-jar.xml")
+				.addAsManifestResource(new File("src/test/resources/META-INF/test-glassfish-ejb-jar.xml"), "glassfish-ejb-jar.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addClasses(
+						LocationCharacteristicsLocal.class, LocationCharacteristicsBean.class,
+						LocationStatusLocal.class, LocationStatusBean.class,
+						LocationLocal.class, LocationBean.class,
+						HandlingUnitLocal.class, HandlingUnitBean.class,
+						PerformanceAuditor.class,
+						MonitoringResource.class
+						);
+
+		LOG.debug(archive.toString(true));
+
+		LOG.trace("<-- createTestArchive()");
+		return archive;
+	}
+
+	@Before
+	public void beforeTest() {
+		LOG.trace("--> beforeTest()");
+		
+		LOG.trace("<-- beforeTest()");		
+	}
+	
+	@After
+	public void afterTest() {
+		LOG.trace("--> afterTest()");
+
+		// Cleanup locations
+		List<Location> locations = locationLocal.getAll();
+		
+		locations.stream().forEach(l -> locationLocal.delete(l));
+		
+		// No need to cleanup location characteristics because its done by cleanup locations (1:1 relation)
+
+		LOG.trace("<-- afterTest()");
+	}
+
+	/**
+	 * Location characteristics is always linked to location
+	 */
+	@Test
+	@InSequence(0)
+	public void create_getById() {
+		LOG.info("--- Test create_getById");
+
+		assertTrue(locationLocal.getAll().isEmpty());
+		assertTrue(locationCharacteristicsLocal.getAll().isEmpty());
+		
+		Location expLocation = new Location("A");
+		
+		locationLocal.create(expLocation);
+		assertEquals(expLocation, locationLocal.getById("A"));
+		
+		LocationCharacteristics locationCharacteristics = locationCharacteristicsLocal.getById(expLocation.getLocationId());		
+		LOG.info("LocationStatus getById: " + locationCharacteristics);
+		
+		// Now check the corresponding LocationCharacteristics
+		assertEquals("A", locationCharacteristics.getLocationId());
+		assertEquals(0, locationCharacteristics.getCapacity());
+		assertEquals(EntityBase.USER_DEFAULT, locationCharacteristics.getUpdateUserId());
+		assertNotNull(locationCharacteristics.getUpdateTimestamp());
+		
+		// This one has not been created
+		assertNull(locationCharacteristicsLocal.getById("B"));
+	}
+	
+	@Test
+	@InSequence(1)
+	public void delete_getById_create() {
+		LOG.info("--- Test delete_getById_create");
+
+		assertTrue(locationLocal.getAll().isEmpty());
+		assertTrue(locationCharacteristicsLocal.getAll().isEmpty());
+		
+		Location expLocation = new Location("A");
+		
+		locationLocal.create(expLocation);
+		assertEquals(expLocation, locationLocal.getById("A"));
+		
+		LOG.info("Location prepared: " + expLocation);
+		LOG.info("LocationCharacteristics implicite prepared: "+ expLocation.getLocationCharacteristics());
+		
+		// Delete the location
+		locationLocal.delete(expLocation);
+		
+		// Now check the location
+		assertNull(locationLocal.getById("A"));
+		assertNull(locationCharacteristicsLocal.getById("A"));
+	}
+}
