bf8e43c21253b5e5fe80ffdee76e61394c12877f
TomBitDe <tbit455674@gmail.com>
2022-10-06 16:26:50 +0200

    Docu extended.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/model/package-info.java
@@ -3,14 +3,27 @@
  *
  * <pre>{@code
  * 
- *      +--------------+                         +----------------+
- *      | HandlingUnit |                         | Location       |
- *      |              |                         |                |
- *      |  id          |                         |  locationId    |
- *      |  location    | <-- 0:1 ------- 0:N --> |  handlingUnits |
- *      |  ...         |                         |  ...           |
- *      +--------------+                         +----------------+
- *      
+ *      +--------------+                         +-----------------+
+ *      | HandlingUnit |                         | Location        |
+ *      |              |                         |                 |
+ *      |  id          |                         |  locationId     |<-----+
+ *      |  location    | <-- 0:1 ------- 0:N --> |  handlingUnits  |      |
+ *      |  ...         |                         |  locationStatus |      |
+ *      +--------------+                         |  ...            |      |      
+ *                                               +-----------------+      |
+ *                                                                        |
+ *                                                                        |
+ *                                                                       1:1
+ *                                                                        |
+ *                                                                        v
+ *                                                                  +----------------+
+ *                                                                  | LocationStatus |
+ *                                                                  |                |
+ *                                                                  |  locationId    |
+ *                                                                  |  errorStatus   |
+ *                                                                  |  ...           |
+ *                                                                  +----------------+
+ *                                                                  
  * }</pre>
  */
 package com.home.simplewarehouse.model;
