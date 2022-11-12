86869cc602d8c45d508f5a7de0d4434762e4d424
TomBitDe <tbit455674@gmail.com>
2022-11-12 08:52:03 +0100

    Better docu.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/timed/package-info.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/timed/package-info.java
@@ -1,7 +1,8 @@
 /**
  * Sample timer controlled session bean classes for tests.<br>
  * <p>
- * Call other session bean for test run in the application server.<br>
+ * Call TimerJpaSessionsBean for all JPA related test runs in the application server.<br>
+ * Call TimerOtherSessionsBean for all other test runs in the application server.<br>
  * <p>
  * This is a sample for a timed bean injecting \@Resource TimeService.
  */
