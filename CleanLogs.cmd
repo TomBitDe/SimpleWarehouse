2d10555085b7c3878fafeed5cdfec58163cad77f
TomBitDe <tbit455674@gmail.com>
2022-11-12 10:43:55 +0100

    Adapted to new Payara Server 5.2022.4 configuration.

--- CleanLogs.cmd
+++ CleanLogs.cmd
@@ -1,5 +1,7 @@
 echo off
 
+set PAYARA_HOME=C:\Users\Tom\Programme\Payara_Server\payara5
+
 dir %PAYARA_HOME%\glassfish\domains\domain1\logs
 echo=
 del %PAYARA_HOME%\glassfish\domains\domain1\logs\*.log*
