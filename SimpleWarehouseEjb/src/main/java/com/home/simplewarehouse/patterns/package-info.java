1e220e49c3951f182b962184a412454d95a1d7b4
TomBitDe <tbit455674@gmail.com>
2023-01-07 10:59:43 +0100

    Better docu.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/patterns/package-info.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/patterns/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Patterns for the project.
  * <p>
- * Sample where the code can be adapted to your needs.
+ * Samples where the code can be adapted to your needs.
  */
 package com.home.simplewarehouse.patterns;
