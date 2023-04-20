16027bfeb5c45e4b2681651580cac7ede98a211f
TomBitDe <tbit455674@gmail.com>
2023-04-20 19:03:27 +0200

    Docu extended.

--- SimpleWarehouseEjb/src/test/java/com/home/simplewarehouse/usecase/package-info.java
+++ SimpleWarehouseEjb/src/test/java/com/home/simplewarehouse/usecase/package-info.java
@@ -5,6 +5,8 @@
  * <ul>
  * <li>Pick a handlingUnit from a RANDOM access location</li>
  * <li>Drop a handlingUnit on a RANDOM access location</li>
+ * <li>Pick a handlingUnit on a FIFO access location</li>
+ * <li>Pick a handlingUnit on a LIFO access location</li>
  * </ul>
  */
 package com.home.simplewarehouse.usecase;
