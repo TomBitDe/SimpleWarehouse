a1d5b95d8a0866282a0c8cb0e18bad8f6440d2f2
TomBitDe <tbit455674@gmail.com>
2023-05-12 21:57:37 +0200

    REST service added.

---
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/rest/locationservice/LocationRestService.java
@@ -0,0 +1,93 @@
+package com.home.simplewarehouse.rest.locationservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.home.simplewarehouse.location.LocationService;
+import com.home.simplewarehouse.model.Location;
+
+/**
+ * RESTful Location handling service.
+ */
+@Path("/LocationRestService")
+@Stateless
+public class LocationRestService {
+	@EJB
+	LocationService locationService;
+
+	/**
+	 * Mandatory default constructor
+	 */
+	public LocationRestService() {
+		super();
+	}
+
+	/**
+     * Get a list of all Locations.
+     *
+     * @return the Location list
+     */
+    @GET
+    @Path("/Content")
+    @Produces({MediaType.APPLICATION_XML})
+    public Response getContent() {
+        List<Location> locationList = locationService.getAll();
+
+        GenericEntity<List<Location>> content
+                = new GenericEntity<List<Location>>(new ArrayList<>(locationList)) {
+        };
+
+        Response response = Response.ok(content).build();
+
+        return response;
+    }
+    
+    /**
+     * Get a list of Locations.
+     *
+     * @param offset the position to start fetching
+     * @param count  the number of fetches to do
+     *
+     * @return the Location list based on offset and count
+     */
+    @GET
+    @Path("/Content/{offset}/{count}")
+    @Produces({MediaType.APPLICATION_XML})
+    public Response getContent(@PathParam("offset") String offset, @PathParam("count") String count) {
+        int intOffset = Integer.parseInt(offset);
+        int intCount = Integer.parseInt(count);
+
+        // Get the configuration entries from the remote call as a List
+        List<Location> locationList = locationService.getAll(intOffset, intCount);
+
+        GenericEntity<List<Location>> content
+                = new GenericEntity<List<Location>>(new ArrayList<>(locationList)) {
+        };
+
+        Response response = Response.ok(content).build();
+
+        return response;
+    }
+
+    /**
+     * Just check if the REST service is available.
+     *
+     * @return the text "Pong"
+     */
+    @GET
+    @Path("/Ping")
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response ping() {
+    	return Response.ok("Pong").build();
+    }
+}
---
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/rest/locationservice/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * REST service for location handling.<br>
+ * <p>
+ * Show, create, update and delete location data.<br>
+ * <p>
+ * Test with your browsers integrated RESTClient functionality. Firefox has a RESTClient add-on.<br>
+ * <br>
+ * - ping (GET http://localhost:8080/war/resources/LocationRestService/Ping)<br>
+ * - content (GET http://localhost:8080/war/resources/LocationRestService/Content)<br>
+ * - content (GET http://localhost:8080/war/resources/LocationRestService/Content/3/5)<br>
+ */
+package com.home.simplewarehouse.rest.locationservice;
