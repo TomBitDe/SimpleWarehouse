09efaad3a8445fc96654c6e83fe6e0d160d5f0d4
TomBitDe <tbit455674@gmail.com>
2023-05-27 11:12:21 +0200

    Added missing methods to make deployment exception free.

--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/utils/configurator/jmxrestview/ConfiguratorMXBean.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/utils/configurator/jmxrestview/ConfiguratorMXBean.java
@@ -2,6 +2,9 @@ package com.home.simplewarehouse.utils.configurator.jmxrestview;
 
 import java.util.Map;
 
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 /**
  * Defines the ConfiguratorMXBean.
  */
@@ -11,5 +14,37 @@ public interface ConfiguratorMXBean {
 	 * 
 	 * @return the Map
 	 */
-	Map<String, String> getConfigurationMap();
+	public Map<String, String> getConfigurationMap();
+	/**
+	 * Gets the Configuration
+	 * 
+	 * @return the Configuration as String
+	 */
+	public String getConfiguration();
+	/**
+	 * Gets the Configuation entry for the given key
+	 * 
+	 * @param key the key value
+	 * 
+	 * @return the entry as String
+	 */
+	public String getEntry(String key);
+	/**
+	 * Add this entry to the Configuration
+	 * 
+	 * @param key the key value for this entry
+	 * @param value the entries value
+	 * @param uriInfo the Uri info
+	 * 
+	 * @return the response
+	 */
+	public Response addEntry(String key, String value, UriInfo uriInfo);
+	/**
+	 * Delete the configuration entry for the given key
+	 * 
+	 * @param key the key value
+	 * 
+	 * @return the response
+	 */
+	public Response deleteEntry(String key);
 }
--- SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/utils/telemetryprovider/monitoring/boundary/MonitoringResourceMXBean.java
+++ SimpleWarehouseEjb/src/main/java/com/home/simplewarehouse/utils/telemetryprovider/monitoring/boundary/MonitoringResourceMXBean.java
@@ -15,15 +15,42 @@ public interface MonitoringResourceMXBean {
 	 */
 	List<Invocation> getSlowestMethods();
 	/**
+	 * Gets a list of the slowest methods
+	 * 
+	 * @param maxResult maximum number of elements in the list
+	 * 
+	 * @return the list
+	 */
+    List<Invocation> getSlowestMethods(int maxResult);
+	/**
 	 * Provide diagnostics data
 	 * @return the data
 	 */
 	Map<String, String> getDiagnostics();
 	/**
+	 * Provide diagnostics data for a given key
+	 * 
+	 * @param key the key value
+	 * 
+	 * @return the data
+	 */
+	String getDiagnosticsForKey(String key);
+	/**
 	 * Provide an exception statistics
 	 * @return the data
 	 */
 	Map<String, Integer> getExceptionStatistics();
+	/**
+	 * Gets the Exception Statistics as String
+	 * 
+	 * @return the Exception Statistics
+	 */
+	String getExceptionStatisticsAsString();
+	/**
+	 * Provide an exception statistics as a String
+	 * @return the data
+	 */
+	String getDiagnosticsAsString();
 	/**
 	 * Gets the number of exceptions
 	 * @return the count
