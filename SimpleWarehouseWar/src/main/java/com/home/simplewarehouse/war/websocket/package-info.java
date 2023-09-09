2cacfb141232e7ead28afb241c2d915d13abf8de
TomBitDe <tbit455674@gmail.com>
2023-09-09 17:38:20 +0200

    Bugfix HTML

--- SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/war/websocket/package-info.java
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/war/websocket/package-info.java
@@ -2,6 +2,6 @@
  * Sample Web Socket implementation.<br>
  * <p>
  * Use this address in your browser http://localhost:8080/war<br>
- * Select 'Go to Locations & Handling Units'<br>
+ * Select 'Go to Locations &amp; Handling Units'<br>
  */
 package com.home.simplewarehouse.war.websocket;
