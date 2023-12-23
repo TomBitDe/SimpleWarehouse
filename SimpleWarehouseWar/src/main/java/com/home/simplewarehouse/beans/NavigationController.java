6403f939ef90618ca476d3196cf3c65cfdd84074
TomBitDe <tbit455674@gmail.com>
2023-12-23 12:21:03 +0100

    Some more samples and use of CSS.

---
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/beans/NavigationController.java
@@ -0,0 +1,62 @@
+package com.home.simplewarehouse.beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.ManagedProperty;
+import javax.inject.Inject;
+import javax.inject.Named;  
+
+@Named
+@RequestScoped
+public class NavigationController implements Serializable {  
+   private static final long serialVersionUID = 1L;
+   
+   @Inject
+   @ManagedProperty("#{param.pageId}") 
+   private String pageId;  
+   
+   public String moveToPage1() {      
+      return "page1";    
+   }  
+   
+   public String moveToPage2() {       
+      return "page2";    
+   }  
+   
+   public String moveToHomePage() {      
+      return "home";    
+   }  
+   
+   public String processPage1() {       
+      return "page";    
+   }  
+   
+   public String processPage2() {       
+      return "page";    
+   } 
+   
+   public String showPage() {       
+      if(pageId == null) {          
+         return "home";       
+      }       
+      
+      if(pageId.equals("1")) {          
+         return "page1";       
+      }
+      else if(pageId.equals("2")) {          
+         return "page2";       
+      }
+      else {          
+         return "home";       
+      }    
+   }  
+   
+   public String getPageId() {       
+      return pageId;    
+   }  
+   
+   public void setPageId(String pageId) {       
+      this.pageId = pageId;   
+   } 
+}
