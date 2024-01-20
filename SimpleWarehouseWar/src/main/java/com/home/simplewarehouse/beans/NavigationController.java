0bacac4b2836c044d229cb323ded8d78e48f12dd
TomBitDe <tbit455674@gmail.com>
2024-01-20 20:37:03 +0100

    Added navigation to page1.

--- SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/beans/NavigationController.java
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/beans/NavigationController.java
@@ -30,86 +30,11 @@ public class NavigationController implements Serializable {
    }
    
    /**
-    * Move to page 1
+    * Move to page
     * 
     * @return the identifier
     */
-   public String moveToPage1() {      
-      return "page1";    
+   public String moveToPage(String page) {      
+       return page + "?faces-redirect=true";    
    }  
-   
-   /**
-    * Move to page 2
-    * 
-    * @return the identifier
-    */
-   public String moveToPage2() {       
-      return "page2";    
-   }  
-   
-   /**
-    * Move to home page
-    * 
-    * @return the identifier
-    */
-   public String moveToHomePage() {      
-      return "home";    
-   }  
-   
-   /**
-    * Process page 1
-    * 
-    * @return the identifier
-    */
-   public String processPage1() {       
-      return "page";    
-   }  
-   
-   /**
-    * Process page 2
-    * 
-    * @return the identifier
-    */
-   public String processPage2() {       
-      return "page";    
-   } 
-   
-   /**
-    * Shows the current page
-    * 
-    * @return the page identifier
-    */
-   public String showPage() {       
-      if(pageId == null) {          
-         return "home";       
-      }       
-      
-      if(pageId.equals("1")) {          
-         return "page1";       
-      }
-      else if(pageId.equals("2")) {          
-         return "page2";       
-      }
-      else {          
-         return "home";       
-      }    
-   }  
-   
-   /**
-    * Gets the page id
-    * 
-    * @return the page id
-    */
-   public String getPageId() {       
-      return pageId;    
-   }  
-   
-   /**
-    * Sets the page id
-    * 
-    * @param pageId the page id
-    */
-   public void setPageId(String pageId) {       
-      this.pageId = pageId;   
-   } 
 }
