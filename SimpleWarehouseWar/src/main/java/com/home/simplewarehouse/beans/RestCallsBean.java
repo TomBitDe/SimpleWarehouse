bd6fa16b329e0d65193bbe24a29454a6ab8869e6
TomBitDe <tbit455674@gmail.com>
2024-01-27 13:34:59 +0100

    Rest access page.

---
+++ SimpleWarehouseWar/src/main/java/com/home/simplewarehouse/beans/RestCallsBean.java
@@ -0,0 +1,51 @@
+package com.home.simplewarehouse.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Bean class that provides REST access (example). 
+ */
+@Named
+@RequestScoped
+public class RestCallsBean {
+	private static final Logger LOG = LogManager.getLogger(RestCallsBean.class);
+	
+    private static final String APPL_CONFIG_REST_SERVICE_URL = "http://localhost:8080/war/resources/ApplConfigRestService/Ping";
+    
+    private String pingResult;
+
+	/**
+	 * Default constructor is not mandatory
+	 */
+    public RestCallsBean() {
+    	super();
+    }
+    
+	/**
+	 * Calls the Ping from ApplConfigRestService
+	 */
+    public void fetchPingResultFromApplConfigRestService() {
+        Client client = ClientBuilder.newClient();
+        pingResult = client.target(APPL_CONFIG_REST_SERVICE_URL)
+                              .request(MediaType.TEXT_PLAIN)
+                              .get(String.class);
+        
+        LOG.debug("Result is [{}]", pingResult);
+    }
+    
+    /**
+     * Gets the ping result
+     * 
+     * @return the result
+     */
+    public String getPingResult() {
+    	return pingResult;
+    }
+}
